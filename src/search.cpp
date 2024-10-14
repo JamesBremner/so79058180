ffc8884a2c39bd0ab9e379d9ff1cb17597e849e1
JamesBremner <james@ravenspoint.com>
2024-10-14 15:11:50 -0400

    cSex

--- src/search.cpp
+++ src/search.cpp
@@ -4,6 +4,62 @@
 #include <sstream>
 #include <algorithm>
 
+/// @brief Integer solution space explorer
+
+class cSex
+{
+
+public:
+    /// @brief set next variable test values
+    /// @param count nunber of variables
+    /// @param max maximum of variable range
+    /// @return true if more to come, false if all done
+
+    bool inc(int count, int max);
+
+    virtual void copy(int *p) = 0;
+    virtual bool isFeasible() = 0;
+
+    // caclulate value of funtion to be optimized with these variable testvalues
+    virtual int optFunVal() = 0;
+
+    int getOptimumValue() const
+    {
+        return myOptValue;
+    }
+    void copyTestVals()
+    {
+        copy(&myVarTestVals[0]);
+    }
+    void copyOptVals()
+    {
+        copy(&myVarBestVals[0]);
+    }
+
+protected:
+    std::vector<int> myVarTestVals;
+    std::vector<int> myVarBestVals;
+    int myRez;
+    int myOptValue;
+
+private:
+    void setVarCount(int count)
+    {
+        myOptValue = 0;
+        if (myVarTestVals.size())
+            return;
+        myVarTestVals.clear();
+        myVarTestVals.resize(count);
+    }
+
+    void setRez(int max)
+    {
+        if (myRez)
+            return;
+        myRez = max / 4;
+    }
+};
+
 typedef std::vector<std::vector<int>> vPec_t;
 
 struct sEmployee
@@ -40,6 +96,58 @@ std::vector<int> theBudgets;
 
 vPec_t vPec;
 
+class cCratePusher : public cSex
+{
+public:
+    bool isFeasible();
+    void copy(int *p);
+    int optFunVal();
+    void constructVariables();
+};
+
+cCratePusher cratePusher;
+
+bool cSex::inc(int count, int max)
+{
+    setVarCount(count);
+    setRez(max);
+
+    while (true)
+    {
+        do
+        {
+            int k = 0;
+            while (true)
+            {
+                int *p = &myVarTestVals[k];
+                *p += myRez;
+                if (*p <= max)
+                {
+                    copyTestVals();
+                    break;
+                }
+                *p = 0;
+                k++;
+                if (k == myVarTestVals.size())
+                {
+                    copyOptVals();
+                    return false;
+                }
+            }
+        } while (!isFeasible());
+
+        // check for improved function value
+        int o = optFunVal();
+        if (o > myOptValue)
+        {
+            myOptValue = o;
+            myVarBestVals = myVarTestVals;
+        }
+    }
+
+    return true;
+}
+
 int Pec(int e, int c)
 {
     return vPec[e][c];
@@ -67,7 +175,7 @@ bool isWithinPayLimit(int e)
     return p < theEmployees[e].myPayLimit;
 }
 
-bool isFeasible()
+bool cCratePusher::isFeasible()
 {
     for (int e = 0; e < theEmployees.size(); e++)
     {
@@ -85,7 +193,7 @@ bool isFeasible()
     return true;
 }
 
-int O()
+int cCratePusher::optFunVal()
 {
     int o = 0;
     for (int e = 0; e < theEmployees.size(); e++)
@@ -96,26 +204,13 @@ int O()
     return o;
 }
 
-bool inc(std::vector<int> &vvv, int base, int res)
+void cCratePusher::constructVariables()
 {
-    int k = 0;
-    while (true)
-    {
-        vvv[k] += res;
-        if (vvv[k] <= base)
-            return true;
-        vvv[k] = 0;
-        k++;
-        if (k == vvv.size())
-            return false;
-    }
-}
-
-void testInc()
-{
-    std::vector<int> vvv(8);
-    while (inc(vvv, 20, 3))
-        std::cout << vvv[0] << " " << vvv[1] << " " << vvv[2] << "\n";
+    vPec_t twoDimVector(
+        theEmployees.size(),
+        std::vector<int>(theBudgets.size(), 0));
+    vPec = twoDimVector;
+    auto vPecOpt = twoDimVector;
 }
 
 void display()
@@ -133,57 +228,17 @@ void display()
         }
         ss << " )\n";
     }
-    ss << "total Distance " << O() << "\n";
+    ss << "total Distance " << cratePusher.optFunVal()
+       << "\n";
 
     std::cout << ss.str();
 }
 
-void Search()
+void cCratePusher::copy(int *psol)
 {
-    // construct test variables
-    vPec_t twoDimVector(
-        theEmployees.size(),
-        std::vector<int>(theBudgets.size(), 0));
-    vPec = twoDimVector;
-    auto vPecOpt = twoDimVector;
-
-    // optimum value so far
-    int o = 0;
-
-    // max variable range
-    const int maxPay = 20;
-
-    // 1D variable values
-    int soldim = theEmployees.size() * theBudgets.size();
-    std::vector<int> vsol(soldim);
-
-    // while more variable values to test
-    while (inc(vsol, maxPay, 3))
-    {
-        // copy variable test values
-        int *psol = (int *)vsol.data();
-        for (int e = 0; e < theEmployees.size(); e++)
-            for (int c = 0; c < theBudgets.size(); c++)
-                vPec[e][c] = *psol++;
-
-        // check feasability
-        if (!isFeasible())
-            continue;
-        
-        // calculate value
-        int v = O();
-
-        // check for new optimum
-        if (v <= o)
-            continue;
-
-        // save new optimum
-        o = v;
-        vPecOpt = vPec;
-    }
-
-    // return optimum found
-    vPec = vPecOpt;
+    for (int e = 0; e < theEmployees.size(); e++)
+        for (int c = 0; c < theBudgets.size(); c++)
+            vPec[e][c] = *psol++;
 }
 
 main()
@@ -194,7 +249,11 @@ main()
     theEmployees.push_back(alice);
     theEmployees.push_back(bob);
     theBudgets = std::vector<int>(4, 20);
+    cratePusher.constructVariables();
 
-    Search();
+    cratePusher.inc(
+        theEmployees.size() * theBudgets.size(),
+        20);
+
     display();
 }
